ee08cb9c86708c6ea6c536f2837620c0124e6f94
Eric Dumazet <edumazet@google.com>
2015-10-23 10:59:16 -0700

    af_unix: do not report POLLOUT on listeners
    poll(POLLOUT) on a listener should not report fd is ready for
    a write().
    
    This would break some applications using poll() and pfd.events = -1,
    as they would not block in poll()
    
    Change-Id: I11008e6dcbf9a2a96accd9916d65d3686cbee759
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Alan Burlison <Alan.Burlison@oracle.com>
    Tested-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -436,7 +436,8 @@ static int unix_dgram_peer_wake_me(struct sock *sk, struct sock *other)
 
 static int unix_writable(const struct sock *sk)
 {
-	return (atomic_read(&sk->sk_wmem_alloc) << 2) <= sk->sk_sndbuf;
+	return sk->sk_state != TCP_LISTEN &&
+	       (atomic_read(&sk->sk_wmem_alloc) << 2) <= sk->sk_sndbuf;
 }
 
 static void unix_write_space(struct sock *sk)
